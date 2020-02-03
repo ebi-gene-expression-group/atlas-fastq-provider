d6a080e32daec7abce6deec3c149a1d2315cd1ef
Jonathan Manning <jmanning@ebi.ac.uk>
2020-02-03 15:08:40 +0000

    Feature/fix ena path (#3)
    * Add fix for ENA codes beyond 10M
    
    * Correct comment
    
    * Fix typo

--- atlas-fastq-provider-functions.sh
+++ atlas-fastq-provider-functions.sh
@@ -12,10 +12,19 @@ get_library_path() {
     local prefix=
     if ! [[ $subDir =~ "ENC" ]] && [[ -z "$forceShortForm" ]] ; then
         local num=${library:3}
-        
+       
+        # ENA pattern is:
+        # 
+        # - 6-digit codes under e.g. SRR123456
+        # - 7-digit codes under e.g. 007/SRR1234567
+        # - 8-digit codes under e.g. 078/SRR12345678
+        #
+        # i.e. we zero-pad to three digits anything after and including the
+        # 10th digit
+
         if [[ "$num" =~ ^[0-9]+$ ]]; then
             if [ $num -gt 1000000 ]; then
-                prefix="00${library:9}/"
+                prefix="$(printf %03d ${library:9})/"
             fi
         fi 
     fi
