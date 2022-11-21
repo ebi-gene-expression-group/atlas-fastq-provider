b47e55a5b943489ed5d083447963287183805050
Anil Thanki <thanki.anil@gmail.com>
2022-11-21 14:00:29 +0000

    adds file extension

--- atlas-fastq-provider-functions.sh
+++ atlas-fastq-provider-functions.sh
@@ -1317,7 +1317,7 @@ fetch_library_files_from_ena() {
     
     for basefile in "${filenames_arr[@]}"; do
         echo "File ${basefile} being copied from ${tempdir} to ${outputDir}"
-        cp -a $tempdir/$basefile $outputDir/
+        cp -a $tempdir/$basefile*fastq.gz $outputDir/
     done;
     
 
